acf6625d69ca6edd8d94eb62d439f95d7e177187
artemis-beta <KXZ186@student.bham.ac.uk>
2017-06-22 14:41:36 +0100

    Changed module name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name                =  'py-tpg'                                      ,
+setup(name                =  'pytpg'                                       ,
       version             =  '0.1.0'                                       ,
       description         =  'Client for TPG API.'                         ,
       url                 =  'http://github.com/artemis-beta/py-tpg'       ,
       author              =  'Kristian Zarebski'                           ,
       author_email        =  'krizar312@yahoo.co.uk'                       ,
       license             =  'MIT'                                         ,
-      packages            =  ['py-tpg']                                    ,
+      packages            =  ['pytpg']                                     ,
       zip_safe            =  False                                         ,
       tests_require       =  ['nose2']                                    
      )
